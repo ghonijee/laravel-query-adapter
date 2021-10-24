ddd30e367ccdde7be84be2736d224840f2e06fbc
ghonijee <akughoni@gmail.com>
2021-10-24 11:01:09 +0700

    update phpunit env for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -32,9 +32,9 @@ class TestCase extends Orchestra
     protected function usesMySqlConnection(Application $app)
     {
         $app->config->set('database.default', 'mysql');
-        $app->config->set('database.connections.mysql.database', 'test_package');
-        $app->config->set('database.connections.mysql.username', 'root');
-        $app->config->set('database.connections.mysql.password', 'asd1234!');
+        $app->config->set('database.connections.mysql.database', $_ENV['DB_DATABASE']);
+        $app->config->set('database.connections.mysql.username', $_ENV['DB_USERNAME']);
+        $app->config->set('database.connections.mysql.password', $_ENV['DB_PASSWORD']);
         $app->config->set('dx-adapter.request.filter', 'filter');
         $app->config->set('dx-adapter.request.select', 'select');
         $app->config->set('dx-adapter.request.order', 'sort');
